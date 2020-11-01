b934d5ffaaeca897805a021d1b926960cf6d4fc7
Dovile Krekenaite <35746175+d0ovi@users.noreply.github.com>
2020-11-01 13:43:02 -0800

    Add files via upload
    Implemented the circles on the right to change the main pages (between almond, cashew and pecan)
    For now, I have 3 bugs on the main page: 
    1. While changing the background colour, the middle part gets dark (is it somehow related to the tutorials code? the colours seem to come from there)
    2. If you press the same circle twice, you will get an empty page.
    3. the right and left part of the images gets flexed together automatically, without you moving the mouse over it (how it suppose to be)

--- OptNuc website/app.js
+++ OptNuc website/app.js
@@ -6,7 +6,6 @@ function init() {
     `radial-gradient(hsl(46, 100%, 94%), hsl(37, 47%, 43%))`,
     `radial-gradient(hsl(18, 96%, 91%),hsl(18, 78%, 40%))`,
   ];
-
   //tracker
   let current = 0;
 
@@ -24,92 +23,45 @@ function init() {
     dot.classList.add('active');
   }
 
-//   function nextSlide(pageNumber) {
-//     const nextPage = pages[pageNumber];
-//     const currentPage = pages[current];
-//     const nextLeft = nextPage.querySelector('.hero .almond-left');
-//     const nextRight = nextPage.querySelector('.hero .almond-right');
-//     const currentLeft = currentPage.querySelector('.hero .almond-left');
-//     const currentRight = currentPage.querySelector('.hero .almond-right');
-//     const nextText = nextPage.querySelector('.details');
-//     const portofolio = document.querySelector('.haveThemAll');
+  function nextSlide(pageNumber) {
+    const nextPage = pages[pageNumber];
+    const currentPage = pages[current];
+    const nextLeft = nextPage.querySelector('.hero .model-left');
+    const nextRight = nextPage.querySelector('.hero .model-right');
+    const currentLeft = currentPage.querySelector('.hero .model-left');
+    const currentRight = currentPage.querySelector('.hero .model-right');
+    const nextText = nextPage.querySelector('.details');
+    const portofolio = document.querySelector('.portofolio');
 
-//     const tl = new TimelineMax();
+    console.log(nextLeft);
+    // console.log(current);
+    current = pageNumber;
+    // console.log(current);
 
-//     tl.fromTo(currentLeft, 0.3, { y: '-10%' }, { y: '-100%' })
-//       .fromTo(currentRight, 0.3, { y: '10%' }, { y: '-100%' }, '-=0.2')
-//       .to(portofolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
-//       .fromTo(
-//         currentPage,
-//         0.3,
-//         { opacity: 1, pointerEvents: 'all' },
-//         { opacity: 0, pointerEvents: 'none' }
-//       )
-//       .fromTo(
-//         nextPage,
-//         0.3,
-//         { opacity: 0, pointerEvents: 'none' },
-//         { opacity: 1, pointerEvents: 'all' },
-//         '-=0.6'
-//       )
-//       .fromTo(nextLeft, 0.3, { y: '-100%' }, { y: '-10%' }, '-=0.6')
-//       .fromTo(nextRight, 0.3, { y: '-100%' }, { y: '10%' }, '-=0.8')
-//       .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
-//       .set(nextLeft, { clearProps: 'all' })
-//       .set(nextRight, { clearProps: 'all' });
+    const tl = new TimelineMax();
 
-//     current = pageNumber;
-//   }
-// }
-
-function nextSlide(pageNumber) {
-  const nextPage = pages[pageNumber];
-  const currentPage = pages[current];
-  const nextLeft = nextPage.querySelector('.hero .almond-left');
-  const nextRight = nextPage.querySelector('.hero .almond-right');
-  const currentLeft = currentPage.querySelector('.hero .almond-left');
-  const currentRight = currentPage.querySelector('.hero .almond-right');
-  const nextText = nextPage.querySelector('.details');
-  const portofolio = document.querySelector('.haveThemAll');
-
-  const tl = new TimelineMax({
-    onStart: function () {
-      slides.forEach((slide) => {
-        slide.style.pointerEvents = 'none';
-      });
-    },
-    onComplete: function () {
-      slides.forEach((slide) => {
-        slide.style.pointerEvents = 'all';
-      });
-    },
-  });
-
-  tl.fromTo(currentLeft, 0.3, { y: '-10%' }, { y: '-100%' })
-    .fromTo(currentRight, 0.3, { y: '10%' }, { y: '-100%' }, '-=0.2')
-    .to(portofolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
-    .fromTo(
-      currentPage,
-      0.3,
-      { opacity: 1, pointerEvents: 'all' },
-      { opacity: 0, pointerEvents: 'none' }
-    )
-    .fromTo(
-      nextPage,
-      0.3,
-      { opacity: 0, pointerEvents: 'none' },
-      { opacity: 1, pointerEvents: 'all' },
-      '-=0.6'
-    )
-    .fromTo(nextLeft, 0.3, { y: '-100%' }, { y: '-10%' }, '-=0.6')
-    .fromTo(nextRight, 0.3, { y: '-100%' }, { y: '10%' }, '-=0.8')
-    .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
-    .set(nextLeft, { clearProps: 'all' })
-    .set(nextRight, { clearProps: 'all' });
-
-  current = pageNumber;
+    tl.fromTo(currentLeft, 0.3, { y: '-10%' }, { y: '-100%' })
+      .fromTo(currentRight, 0.3, { y: '10%' }, { y: '-100%' }, '-=0.2')
+      .to(portofolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
+      .fromTo(
+        currentPage,
+        0,
+        { opacity: 1, pointerEvents: 'all' },
+        { opacity: 0, pointerEvents: 'none' }
+      )
+      .fromTo(
+        nextPage,
+        0.3,
+        { opacity: 0, pointerEvents: 'none' },
+        { opacity: 1, pointerEvents: 'all' },
+        '-=0.6'
+      )
+      .fromTo(nextLeft, 0.3, { y: '-100%' }, { y: '-10%' }, '-=0.6')
+      .fromTo(nextRight, 0.3, { y: '-100%' }, { y: '10%' }, '-=0.8')
+      .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
+      .set(nextLeft, { clearProps: 'all' })
+      .set(nextRight, { clearProps: 'all' });
+  }
 }
-
-
 
 init();
