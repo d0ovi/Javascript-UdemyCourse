ee4eaf63f14ebaa3175aaa6033be8deb6a63aea9
Dovile Krekenaite <35746175+d0ovi@users.noreply.github.com>
2020-11-01 14:24:27 -0800

    Add files via upload

--- OptNuc website/app.js
+++ OptNuc website/app.js
@@ -2,9 +2,9 @@ function init() {
   const slides = document.querySelectorAll('.slide');
   const pages = document.querySelectorAll('.page');
   const backgrounds = [
-    `radial-gradient(hsl(30, 58%, 88%), hsl(25, 84%, 25%))`,
-    `radial-gradient(hsl(46, 100%, 94%), hsl(37, 47%, 43%))`,
-    `radial-gradient(hsl(18, 96%, 91%),hsl(18, 78%, 40%))`,
+    `radial-gradient(#f2e0cf, #75370a)`,
+    `radial-gradient(#fff8e0, #a17a3a)`,
+    `radial-gradient(#fedfd2, #b64616)`,
   ];
   //tracker
   let current = 0;
@@ -33,34 +33,38 @@ function init() {
     const nextText = nextPage.querySelector('.details');
     const portofolio = document.querySelector('.portofolio');
 
-    console.log(nextLeft);
-    // console.log(current);
     current = pageNumber;
-    // console.log(current);
+    console.log(current);
+    console.log(currentLeft);
+    console.log(currentRight);
+    console.log(nextLeft);
+    console.log(nextRight);
 
     const tl = new TimelineMax();
-
-    tl.fromTo(currentLeft, 0.3, { y: '-10%' }, { y: '-100%' })
-      .fromTo(currentRight, 0.3, { y: '10%' }, { y: '-100%' }, '-=0.2')
-      .to(portofolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
-      .fromTo(
-        currentPage,
-        0,
-        { opacity: 1, pointerEvents: 'all' },
-        { opacity: 0, pointerEvents: 'none' }
-      )
-      .fromTo(
-        nextPage,
-        0.3,
-        { opacity: 0, pointerEvents: 'none' },
-        { opacity: 1, pointerEvents: 'all' },
-        '-=0.6'
-      )
-      .fromTo(nextLeft, 0.3, { y: '-100%' }, { y: '-10%' }, '-=0.6')
-      .fromTo(nextRight, 0.3, { y: '-100%' }, { y: '10%' }, '-=0.8')
-      .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
-      .set(nextLeft, { clearProps: 'all' })
-      .set(nextRight, { clearProps: 'all' });
+    if (currentPage !== nextPage) {
+      tl.fromTo(currentLeft, 0.3, { y: '-10%' }, { y: '-100%' })
+        .fromTo(currentRight, 0.3, { y: '10%' }, { y: '-100%' }, '-=0.2')
+        .to(portofolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
+        .fromTo(
+          currentPage,
+          0,
+          { opacity: 1, pointerEvents: 'all' },
+          { opacity: 0, pointerEvents: 'none' }
+        )
+        .fromTo(
+          nextPage,
+          0.3,
+          { opacity: 0, pointerEvents: 'none' },
+          { opacity: 1, pointerEvents: 'all' },
+          '-=0.6'
+        )
+        .fromTo(nextLeft, 0.3, { y: '-100%' }, { y: '-10%' }, '-=0.6')
+        .fromTo(nextRight, 0.3, { y: '-100%' }, { y: '10%' }, '-=0.8')
+        .fromTo(nextText, 0.3, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
+        //I need to clearprops so the hover function would work again
+        .set(nextLeft, { clearProps: 'all' })
+        .set(nextRight, { clearProps: 'all' });
+    }
   }
 }
 
